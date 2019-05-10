77e1a35303b0adf0455c5277a3a0b32dbc18c296
Tiana Johnson <tiana.marie013@gmail.com>
2019-05-10 10:09:58 -0500

    database build

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
@@ -16,3 +17,18 @@ CREATE TABLE "user_type" (
     "passive_user" BOOLEAN DEFAULT false,
 	"user_id" INTEGER REFERENCES "user"
     );
+
+CREATE TABLE "user-title" (
+	"id" SERIAL PRIMARY KEY,
+	"builder" BOOLEAN DEFAULT false,
+	"fitter" BOOLEAN DEFAULT false,
+	"client" BOOLEAN DEFAULT false
+	);
+	
+CREATE TABLE "user_info" (
+	"id" SERIAL PRIMARY KEY,
+	"user-name" VARCHAR NOT NULL,
+	"user-pronouns" TEXT,
+	"user-title" INTEGER REFERENCES "user-title",
+	"user-type-id" INTEGER REFERENCES "user_type"
+	);
