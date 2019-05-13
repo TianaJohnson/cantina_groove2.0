654365ff23cc858e9ecbb9b5aeb0459cb1b71abb
Tiana Johnson <tiana.marie013@gmail.com>
2019-05-13 10:17:36 -0500

    database build

--- database.sql
+++ database.sql
@@ -42,3 +42,29 @@ CREATE TABLE "client_contact_info" (
 	"cust_notes" VARCHAR (1000),
 	"is_active" BOOLEAN DEFAULT TRUE,
 	"date_activated" DATE NOT NULL DEFAULT CURRENT_DATE);
+
+	-- may need to be edited 
+	CREATE TABLE "project"(
+	"id" SERIAL PRIMARY KEY,
+	"client_id" INTEGER REFERENCES "client_contact_info",
+	
+	);
+
+CREATE TABLE "rider_metrix" (
+	"id" BINARY NOT NULL AUTO_INCREMENT,
+	"rider_height" TEXT NOT NULL,
+	"pubic_bone_height/inseam" TEXT NOT NULL,
+	"method_for_inseam_measurement" TEXT NOT NULL,
+	"sternal_notch_height/body Length" TEXT NOT NULL,
+	"arm_length_right_ac_cf" TEXT NOT NULL,
+	"arm_length_left_ac_cf" TEXT NOT NULL,
+	"forearm_length_right_bl_cf" TEXT NOT NULL,
+	"forearm_length_left_bl_cf" TEXT NOT NULL,
+	"lower_leg_length_right" TEXT NOT NULL,
+	"lower_leg_length_left" TEXT NOT NULL,
+	"shoulder_width_ac_to_ac" TEXT NOT NULL,
+	"shoulder_width_os_to_os" TEXT NOT NULL,
+	"cycling_shoe_size" TEXT NOT NULL,
+	"distance_toe_to_center_of_cleat" TEXT NOT NULL,
+	"project_id" NTEGER REFERENCES "project"
+);
