7da23f84f06284e4367e1c98129e1939bd1b0bcc
Tiana Johnson <tiana.marie013@gmail.com>
2019-06-19 20:00:57 -0500

    today I am sick

--- server/routes/customer.router.js
+++ server/routes/customer.router.js
@@ -54,6 +54,7 @@ router.post('/', (req, res, next) => {
     })
 
 //unable to fetch from server..
+//today I am sick
     //This is where I will have to merge the project and customer info
 // or maybe I just do this in the project router....
     router.get('/', (req, res) => {
