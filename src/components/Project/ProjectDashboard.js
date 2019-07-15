a61265a694bdaed9e10b7c446e8ebcd05c2430cf
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-14 22:52:20 -0500

    not always

--- src/components/Project/ProjectDashboard.js
+++ src/components/Project/ProjectDashboard.js
@@ -15,7 +15,7 @@ import './project.css';
 // This needs to be updated to reflect the project Dash 
 // admin is a temp from old Cantina.
 // gunna get back on it, twas my birthday
- 
+// its no always going to be this way
 
 //Class constructor
 class projectDash extends Component {
